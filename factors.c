2e38c6e28f42642f0269cc76d3f856e369f7ad97
youssefhammani <youssefhammani04@gmail.com>
2023-10-29 09:50:54 -0700

    Factorize all the things!

--- factors.c
+++ factors.c
@@ -1,54 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-/*
- * factors - the given number `num` into two smaller numbers
- * and prints the factorization
- * @num: The number to be factorized
-*/
-void factorize(int num)
+
+/**
+ * factors - The function factorize a number
+ * @buffer: pointer to the address of the number
+ *
+ * Return: int
+ */
+int factors(char *buffer)
 {
-	int i;
+	uint32_t nbr;
+	uint32_t idx;
 
-	for (i = 2; i <= num / 2; i++)
+	nbr = atoi(buffer);
+	idx = 1;
+
+	while (++idx < nbr)
 	{
-		if (num % i == 0)
+		if (nbr % idx == 0)
 		{
-			printf("%d=%d*%d\n", num, i, num / i);
-			return;
+			printf("%d=%d*%d\n", nbr, nbr / idx, idx);
+			break;
 		}
 	}
-}
-
-/*
- * main - function that reads numbers from a file and factors each number.
- * @argc: The number of command-line arguments.
- * @argv: An array containing the command-line arguments.
- * Returns: 0 on success or 1 on error.
-*/
-int main(int argc, char *argv[])
-{
-	if (argc != 2)
-	{
-		printf("Usage: %s <file>\n", argv[0]);
-		return (1);
-	}
-
-	FILE *file = fopen(argv[1], "r");
-
-	if (file == NULL)
-	{
-		printf("File not found.\n");
-		return (1);
-	}
-
-	int num;
-
-	while (fscanf(file, "%d", &num) == 1)
-	{
-		factorize(num);
-	}
-
-	fclose(file);
 
 	return (0);
 }
+
+/**
+ * main - main function
+ * @ac: number of command line arguments.
+ * @av: array that contains the program command line arguments.
+ *
+ *
+ * Return: void
+ */
+int main(int ac, char *av[])
+{
+	FILE *fd;
+	size_t count = 0;
+	ssize_t line;
+	char *buffer = NULL;
+
+	if (ac != 2)
+	{
+		fprintf(stderr, "Usage: factor <filename>\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fd = fopen(av[1], "r");
+
+	if (fd == NULL)
+	{
+		fprintf(stderr, "Error: can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((line = getline(&buffer, &count, fd)) != -1)
+	{
+		factors(buffer);
+	}
+
+	free(buffer);
+	fclose(fd);
+
+	return (0);
+}
+
